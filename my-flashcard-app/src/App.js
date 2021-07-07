cd034d0801597e142870e471e20ddd5055d6f89f
EmilbekDzhumakeev <emilbek.dzhumakeev17@gmail.com>
2021-07-07 17:16:30 -0400

    created app.css able to display cards but still cant flip them

--- my-flashcard-app/src/App.js
+++ my-flashcard-app/src/App.js
@@ -1,10 +1,43 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import FlashcardList from './FlashcardList';
+import './app.css' 
+import axios from 'axios'
 
 function App() {
   const [flashcards, setFlashcards] = useState(Sample_flashcards)
+
+
+  useEffect(() => {
+  axios
+        .get('https://opentdb.com/api.php?amount=10')
+        .then (res => {
+          setFlashcards(res.data.results.map((questionItem, index) => {
+            const answer  = decodeString(questionItem.correct_answer)
+            const options = [
+              ...questionItem.incorrect_answers.map(a => decodeString(a)),
+               answer
+              ]
+            return {
+             id: '${index}-${Date.now()}',
+              question: decodeString(questionItem.question), 
+              answer: answer,
+             options: options.sort(() => Math.random() - .5)
+            }
+          }))
+         
+        })
+      },[]) 
+
+      function decodeString(str){
+        const textArea  = document.createElement('textarea')
+        textArea.innerHTML= str 
+        return textArea.value
+      }
+
   return (
-   <FlashcardList flashcards = {flashcards}/>
+    <div className = "container">
+   <FlashcardList flashcards = {flashcards}/> 
+   </div>
   );
 }
  
